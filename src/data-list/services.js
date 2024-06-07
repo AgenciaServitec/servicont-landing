7e6df87644b4ffa825f261bdef1f3b1e7ff89382
Ignacio2906 <ignacio.malcer@gmail.com>
2024-06-07 11:38:08 -0500

    services section

---
+++ src/data-list/services.js
@@ -0,0 +1,32 @@
+export const services = {
+  accounting: [
+    "Servicios contables.",
+    "Presentaciones Tributarias.",
+    "Asesoría Contable – Tributaria.",
+    "Control Interno.",
+    "Actualización de Registros y Libros Contables.",
+    "Preparación de Estados Financieros.",
+    "Declaración de Impuestos.",
+  ],
+  administration: [
+    "Outsourcing Laboral y de Recursos Humanos.",
+    "Servicios Laborales y de Recursos Humanos.",
+    "Outsourcing Administrativo Financiero.",
+  ],
+  reorganization: [
+    "Auditoria de Estados Financieros.",
+    "Auditoria Operativa.",
+    "Auditoria Tributaria.",
+    "Auditoria Interna.",
+    "Exámenes Especiales de Auditoria.",
+  ],
+  others: [
+    "Asesoría Jurídica en lo Civil.",
+    "Asesoría en la Ley General de Sociedades.",
+    "Asesoría ante Registros Públicos.",
+    "Asesoría ante Notarías.",
+    "Negociación y Administración de Contratos",
+    "Evaluación de Puestos de Trabajo.",
+    "Cobranzas difíciles de Letras, Facturas, etc.",
+  ],
+};
