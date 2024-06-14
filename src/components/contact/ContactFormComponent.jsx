7232f44020e7ef322b0396666122b3457c2f0a9b
noel <admin@agenciaservitec.com>
2024-06-14 16:45:00 -0500

    Fix: send email contact

---
+++ src/components/contact/ContactFormComponent.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+
+const FormContact = () => {
+  const [loading, setLoading] = useState(false);
+  const [responseMessage, setResponseMessage] = useState("");
+
+  async function submit(e) {
+    try {
+      e.preventDefault();
+      setLoading(true);
+
+      const formData = new FormData(e.target);
+
+      const response = await fetch("/api/contact", {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await response.json();
+
+      if (data.ok) {
+        setResponseMessage(data.message);
+      }
+
+      window.location.href = "/success";
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <form onSubmit={submit} noValidate>
+      <label htmlFor="name">
+        Nombres
+        <input type="text" id="name" name="name" autoComplete="name" required />
+      </label>{" "}
+      <br />
+      <label htmlFor="email">
+        Email
+        <input
+          type="email"
+          id="email"
+          name="email"
+          autoComplete="email"
+          required
+        />
+      </label>{" "}
+      <br />
+      <label htmlFor="phoneNumber">
+        Teléfono
+        <input
+          type="number"
+          id="phoneNumber"
+          name="phoneNumber"
+          autoComplete="phoneNumber"
+          required
+        />
+      </label>
+      <br />
+      <label htmlFor="message">
+        Mensaje
+        <textarea id="message" name="message" autoComplete="off" required />
+      </label>
+      <button
+        className={loading ? "disabled" : ""}
+        disabled={loading}
+        style={{
+          minWidth: "300px",
+          padding: "1em",
+          border: "none",
+          pointerEvents: `${loading ? "none" : ""}`,
+          background: `${loading ? "yellow" : ""}`,
+        }}
+      >
+        Enviar
+      </button>
+      {responseMessage && <p>{responseMessage}</p>}
+    </form>
+  );
+};
+
+export default FormContact;
--- src/layout/Drawer.jsx
+++ src/layout/Drawer.jsx
@@ -1,83 +1,96 @@
 import { useVisibleDrawer } from "../hooks/index.js";
 
 const Drawer = ({ children }) => {
-    const { visibleDrawer, showDrawer, hiddenDrawer } = useVisibleDrawer();
+  const { visibleDrawer, showDrawer, hiddenDrawer } = useVisibleDrawer();
 
-    return (
-        <>
-            <button
-                style={{ border: "none", background: "none" }}
-                onClick={showDrawer}
-            >
-                {children}
-            </button>
+  return (
+    <>
+      <button
+        style={{ border: "none", background: "none" }}
+        onClick={showDrawer}
+      >
+        {children}
+      </button>
 
-            <section
-                style={{
-                    position: "fixed",
-                    zIndex: "300",
-                    width: "100vw",
-                    height: "100vh",
-                    top: "0",
-                    right: "0",
-                    transition: "all 0.3s ease-in-out ",
-                    transform: visibleDrawer ? " translateX(0%)" : " translateX(100%)",
-                    display: "grid",
-                    gridTemplateColumns: "25% 1fr",
-                }}
-            >
-                <div onClick={hiddenDrawer} style={{
-                    background: "rgba(13,119,193, .2)"
-                }}></div>
-                <article
-                    style={{ position: "relative", background: "rgb(13,119,193)", padding: "2em 1em" }}
-                >
-                    <span style={{position: "absolute", top: "1em", right: "1em", color:"white"}} onClick={hiddenDrawer}>X</span>
-                    <ul style={{ listStyle: "none" }}>
-                        <ItemLi text="Inicio" onHiddenDrawer={hiddenDrawer} />
-                        <ItemLi
-                            text="Servicio"
-                            path="/service"
-                            onHiddenDrawer={hiddenDrawer}
-                        />
-                        <ItemLi
-                            text="Nosotros"
-                            path="/about-us"
-                            onHiddenDrawer={hiddenDrawer}
-                        />
-                        <ItemLi
-                            text="Planes"
-                            path="/plans"
-                            onHiddenDrawer={hiddenDrawer}
-                        />
+      <section
+        style={{
+          position: "fixed",
+          zIndex: "300",
+          width: "100vw",
+          height: "100vh",
+          top: "0",
+          right: "0",
+          transition: "all 0.3s ease-in-out ",
+          transform: visibleDrawer ? " translateX(0%)" : " translateX(100%)",
+          display: "grid",
+          gridTemplateColumns: "25% 1fr",
+        }}
+      >
+        <div
+          onClick={hiddenDrawer}
+          style={{
+            background: "rgba(13,119,193, .2)",
+          }}
+        ></div>
+        <article
+          style={{
+            position: "relative",
+            background: "rgb(13,119,193)",
+            padding: "2em 1em",
+          }}
+        >
+          <span
+            style={{
+              position: "absolute",
+              top: "1em",
+              right: "1em",
+              color: "white",
+            }}
+            onClick={hiddenDrawer}
+          >
+            X
+          </span>
+          <ul style={{ listStyle: "none" }}>
+            <ItemLi text="Inicio" onHiddenDrawer={hiddenDrawer} />
+            <ItemLi
+              text="Servicio"
+              path="/service"
+              onHiddenDrawer={hiddenDrawer}
+            />
+            <ItemLi
+              text="Nosotros"
+              path="/about-us"
+              onHiddenDrawer={hiddenDrawer}
+            />
+            <ItemLi text="Planes" path="/plans" onHiddenDrawer={hiddenDrawer} />
 
-                        <ItemLi
-                            text="Contacto"
-                            path="/contact"
-                            onHiddenDrawer={hiddenDrawer}
-                        />
-                    </ul>
-                </article>
-            </section>
-        </>
-    );
+            <ItemLi
+              text="Contacto"
+              path="/contact"
+              onHiddenDrawer={hiddenDrawer}
+            />
+          </ul>
+        </article>
+      </section>
+    </>
+  );
 };
 
 export default Drawer;
 
 const ItemLi = ({ text, path = "/", onHiddenDrawer }) => (
-    <li
-        style={{ padding: "0.3em 0", fontSize: "1.5em" }}
-        onClick={onHiddenDrawer}
+  <li
+    style={{ padding: "0.3em 0", fontSize: "1.5em" }}
+    onClick={onHiddenDrawer}
+  >
+    <a
+      href={path}
+      style={{
+        textDecoration: "none",
+        color: "white",
+      }}
     >
-        <a
-            href={path}
-            style={{
-                textDecoration: "none",
-                color: "white",
-            }}
-        >
-            {text}
-        </a>
-    </li>
+      {text}
+    </a>
+  </li>
 );
