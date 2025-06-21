dd898e1ad5c8ce6c933599447c0debbe5ba0e808
Angel Gala <galafloresangelemilio@gmail.com>
2025-06-20 19:47:32 -0500

    ...

--- src/components/ui/index.js
+++
@@ -1,3 +0,0 @@
-export { default as IconFontAwesome } from "./IconFontAwesome.astro";
-export { default as ButtonFloatingWhatsapp } from "./ButtonFloatingWhatsapp.astro";
-export  {default as ButtonFloatingContact} from "./ButtonFloatingContact.astro";
