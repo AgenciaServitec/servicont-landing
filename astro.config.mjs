2c100d531df68b69d426d22e899b9f148765f9f7
Angel Gala <galafloresangelemilio@gmail.com>
2025-06-25 13:24:07 -0500

    refactored landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,15 @@
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify/functions";
-import partytown from "@astrojs/partytown";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-    icon(),
-  ],
-
+  integrations: [icon()],
   build: {
     inlineStylesheets: "always",
   },
-
   compressHTML: true,
   prefetch: true,
-  adapter: netlify(),
-
   vite: {
     plugins: [tailwindcss()],
   },
